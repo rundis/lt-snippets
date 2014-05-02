23e751b09f927e2826a6a4083868f7f864fbc94c
Magnus Rundberget <mrundberget@hotmail.com>
2014-05-02 17:17:43 +0200

    Allow wrapping around current line even though not selected

--- helpers.js
+++ helpers.js
@@ -8,9 +8,31 @@ window.snip$ = (function() {
 
   function currPath() { return lt.objs.tabs.__GT_path(currEd()); }
 
+  function currFileName() {
+    var p = currPath();
+    return p ?  path.basename(p) : null;
+  }
+
+  function currFileNameSansExt() {
+    var f = currFileName();
+    return f ? f.replace(path.extname(f), "") : null;
+  }
+
   function hasSelection() { return lt.objs.editor.selection_QMARK_.call(null, currEd()); }
 
-  function selection () { return lt.objs.editor.selection(currEd()); }
+  function selection() { return lt.objs.editor.selection(currEd()); }
+
+  function selectLine() {
+    var ed = currEd();
+    var cmEd = lt.objs.editor.__GT_cm_ed.call(null, ed);
+
+    var lineNo = cmEd.getCursor().line;
+    var line = cmEd.getLine(lineNo);
+    var length = line.length;
+
+    cmEd.setSelection({line: lineNo, ch: 0}, {line: lineNo, ch: length});
+    cmEd.setExtending(false);
+  }
 
   function cutSelection() { lt.objs.editor.cut(currEd()); }
 
@@ -20,13 +42,23 @@ window.snip$ = (function() {
       cutSelection();
       return text;
     }
-  };
+  }
+
+  function wrapSelectionEager() {
+    if(!hasSelection()) {
+      selectLine();
+    }
+    return wrapSelection();
+  }
 
 
   return {
     currPath: currPath,
+    currFileName: currFileName,
+    currFileNameSansExt: currFileNameSansExt,
     path: path,
-    wrapSelection: wrapSelection
+    wrapSelection: wrapSelection,
+    wrapSelectionEager: wrapSelectionEager
 
   }
 
